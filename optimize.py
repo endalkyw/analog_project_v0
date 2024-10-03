a52c141adde23ca78f3a036ce319198f143d7b42
endalkyw <endalkyw@gmail.com>
2024-10-03 12:26:08 -0500

    c 3 pc

--- optimize.py
+++ optimize.py
@@ -22,7 +22,7 @@ gm_1 = gm_3 = gm_5 = l_1  = l_3  = l_5  = cdd_3 = cdd_5 = c_self = Itail = vds_0
 
 t_spec = { 
   "gain": 20,
-  "ugf": 40e6,
+  "ugf": 10e6,
   "cmrr": 20,
   "sr": 2,
   "p": 100e-6,
@@ -52,34 +52,34 @@ def power_func(TE, gm_3, vdd): # objective function
 
 
 # constraints 
-def con_1(TE,gm_3, c_self): # slew rate
+def con_sr(TE,gm_3, c_self): # slew rate
     return (2*gm_3/(TE[1]*(t_spec["cl"] + c_self)))*1e-6 - t_spec["sr"]
 
-def con_2(TE, l_3, l_5): # gain
+def con_gain(TE, l_3, l_5): # gain
     return 20*np.log10(TE[1]/(l_3+l_5)) - t_spec["gain"]
 
-def con_3(TE, l_1, l_3, l_5):   # cmrr
+def con_cmrr(TE, l_1, l_3, l_5):   # cmrr
     return 20*np.log10((TE[1]*TE[2])/(l_1*(l_3+l_5)))-t_spec["cmrr"]
 
-def con_4(TE, gm, l_3, l_5 , cdd_3, cdd_5): # bw
+def con_bw(TE, gm, l_3, l_5 , cdd_3, cdd_5): # bw
     return (gm/(TE[1]*2*np.pi*(t_spec["cl"] + cdd_3 + cdd_5)))*(l_5 + l_3) - t_spec["bw"]
 
-def con_5(TE, cgg_5, Itail): # the non dominant pole should be greater than the GBW
-    return TE[2]*(Itail/2)/(2*cgg_5) - 1.5*t_spec["ugf"]
+def con_p2(TE, gm_3, cgg_5): # the non dominant pole should be greater than the GBW
+    return (TE[2]/TE[1])*((gm_3)/(2*np.pi*cgg_5)) - 2.7*t_spec["ugf"]
 
 # All the mosfets must operate in saturation region
-def con_6(TE, vds_0): 
+def con_vdsat0(TE, vds_0): 
     return vds_0 - 2/TE[0] 
 
-def con_7(TE, vds_1): 
+def con_vdsat1(TE, vds_1): 
     return vds_1 - 2/TE[1]
 
-def con_8(TE, vsd_2):
+def con_vdsat2(TE, vsd_2):
     return vsd_2 - 2/TE[2]
 
 
 def gm_id_optimizer(xk, final=False):
-    ota = initialize(5e6)
+    ota = initialize(0)
     global gm_1, gm_3, gm_5, l_1, l_3, l_5, cdd_3, cdd_5, it, spec_i, Itail, cgg_3, cgg_5,vds_0, vds_1, vsd_2 
 
     vcm     = 0.6
@@ -107,16 +107,16 @@ def gm_id_optimizer(xk, final=False):
     c_self = (cgg_3)/2 +  cdd_3 + cdd_5
 
     it += 1
-    f = "optimization.log"
+    f = "optimization.csv"
 
 
-    Gain     = (con_2(xk, l_3, l_5) + t_spec['gain'])[0]
+    Gain     = (con_gain(xk, l_3, l_5) + t_spec['gain'])[0]
     UGF      = (other_res['ugf'])[0]
-    BW       = (con_4(xk, gm_3, l_3, l_5, cdd_3, cdd_5)+ t_spec['bw'])[0]
+    BW       = (con_bw(xk, gm_3, l_3, l_5, cdd_3, cdd_5)+ t_spec['bw'])[0]
     Power    = (power_func(xk, gm_3, 1.2))[0]
-    CMRR     = (con_3(xk, l_1, l_3, l_5) + t_spec['cmrr'])[0]
-    SR       = (con_1(xk,gm_3, c_self) + t_spec['sr'])[0]
-    Pole_2   = (con_5(xk,cgg_5,Itail) + 1.5*t_spec['ugf'])[0]
+    CMRR     = (con_cmrr(xk, l_1, l_3, l_5) + t_spec['cmrr'])[0]
+    SR       = (con_sr(xk,gm_3, c_self) + t_spec['sr'])[0]
+    Pole_2   = (con_p2(xk,gm_3, cgg_5) + 2.7*t_spec['ugf'])[0]
     vds      = [vds_0, vds_1, vsd_2]
     vds_sat  = [2/xk[0], 2/xk[1], 2/xk[2]]
     TEs      = xk
@@ -137,20 +137,22 @@ def gm_id_optimizer(xk, final=False):
             return True
         return False
     print("f gain   cmrr    sr  pole2   sat0    sat1    sat2")
-    print(ch(con_2(xk, l_3, l_5)), ch(con_3(xk, l_1, l_3, l_5)), ch(con_1(xk,gm_3, c_self)), ch(con_5(xk,cgg_5,Itail)), 
-          ch(con_6(xk, vds_0)), ch(con_7(xk, vds_1)), ch(con_8(xk, vsd_2)))
-    
-    print(" ")
+    print(ch(con_gain(xk, l_3, l_5)), ch(con_cmrr(xk, l_1, l_3, l_5)), ch(con_sr(xk,gm_3, c_self)), ch(con_p2(xk, gm_3, cgg_5)),
+          ch(con_vdsat0(xk, vds_0)), ch(con_vdsat1(xk, vds_1)), ch(con_vdsat2(xk, vsd_2)))
 
+    # print(con_p2(xk, gm_3, cgg_5))
+    # print((xk[2]/xk[1])*(gm_3/(2*np.pi*cgg_5)) - t_spec["ugf"])
+    # print((xk[2]/xk[1])*(gm_3/(2*np.pi*cgg_5)) - t_spec["ugf"])
+    # print(" ")
     
     # write_log_file(f, f" iteration {it} ----", 'a')
-    # write_log_file(f, f" Gain    {con_2(xk, l_3, l_5) + t_spec['gain']}", 'a')
+    # write_log_file(f, f" Gain    {con_gain(xk, l_3, l_5) + t_spec['gain']}", 'a')
     # write_log_file(f, f" UGF     {other_res['ugf']}", 'a')
-    # write_log_file(f, f" BW      {con_4(xk, gm_3, l_3, l_5, CL, cdd_3, cdd_5)+ t_spec['bw']}", 'a')
+    # write_log_file(f, f" BW      {con_bw(xk, gm_3, l_3, l_5, CL, cdd_3, cdd_5)+ t_spec['bw']}", 'a')
     # write_log_file(f, f" Power   {power_func(xk, gm_3, 1.2)}", 'a')
-    # write_log_file(f, f" CMRR    {con_3(xk, l_1, l_3, l_5) + t_spec['cmrr']}", 'a')
-    # write_log_file(f, f" SR      {con_1(xk,gm_3,CL, c_self) + t_spec['sr']}", 'a')
-    # write_log_file(f, f" Pole_2  {con_5(xk,cgg_5,Itail) + 1.5*t_spec['ugf']}", 'a')
+    # write_log_file(f, f" CMRR    {con_cmrr(xk, l_1, l_3, l_5) + t_spec['cmrr']}", 'a')
+    # write_log_file(f, f" SR      {con_sr(xk,gm_3,CL, c_self) + t_spec['sr']}", 'a')
+    # write_log_file(f, f" Pole_2  {con_p2(xk,cgg_5,Itail) + 1.5*t_spec['ugf']}", 'a')
     # write_log_file(f, f" vds     {vds_0} {vds_1}, {vsd_2}", 'a')
     # write_log_file(f, f" vds_sat {2/xk[0]}, {2/xk[1]}, {2/xk[2]}", 'a')
     # write_log_file(f, f" ", 'a')    
@@ -165,26 +167,25 @@ def gm_id_optimizer(xk, final=False):
 
 def run_optimization():
     global gm_1, gm_3, gm_5, l_1, l_3, l_5, cdd_3, cdd_5, c_self, vds_0, vds_1, vsd_2
-    TE0 = [18, 23, 17]
+    TE0 = [15, 15, 15]
     cons = (
-        {'type': 'ineq', 'fun': con_1, 'args': (gm_3, c_self)},
-        {'type': 'ineq', 'fun': con_2, 'args': (l_3, l_5)},
-        {'type': 'ineq', 'fun': con_3, 'args': (l_1, l_3, l_5)},
-        {'type': 'ineq', 'fun': con_5, 'args': (cgg_5, Itail)},
-        {'type': 'ineq', 'fun': con_6, 'args': ([vds_0])},
-        {'type': 'ineq', 'fun': con_7, 'args': ([vds_1])},
-        {'type': 'ineq', 'fun': con_8, 'args': ([vsd_2])}
+        {'type': 'ineq', 'fun': con_sr, 'args': (gm_3, c_self)},
+        {'type': 'ineq', 'fun': con_gain, 'args': (l_3, l_5)},
+        {'type': 'ineq', 'fun': con_cmrr, 'args': (l_1, l_3, l_5)},
+        # {'type': 'ineq', 'fun': con_p2, 'args': (gm_3, cgg_5)},
+        {'type': 'ineq', 'fun': con_vdsat0, 'args': ([vds_0])},
+        {'type': 'ineq', 'fun': con_vdsat1, 'args': ([vds_1])},
+        {'type': 'ineq', 'fun': con_vdsat2, 'args': ([vsd_2])}
     )
 
 
 
     # specifying the gm/id bounds
-    bounds = [(5,  15), (18, 25), (5,  15)] 
+    bounds = [(5,  20), (18, 25), (5,  20)]
 
     # trust-constr SLSQP
-    result = minimize(power_func, TE0,args= (gm_3, 1.2), method='trust-constr', constraints=cons, bounds = bounds,
-                      callback = gm_id_optimizer, options={'disp': True, 'maxiter': 1000, 'gtol': 1e-6}
-)
+    result = minimize(power_func, TE0, args=(gm_3, 1.2), method='trust-constr', constraints=cons, bounds=bounds,
+                      callback=gm_id_optimizer, options={'disp': True, 'maxiter': 1000, 'gtol': 1e-9})
     return result
 
 def hspice_verification(inp):
@@ -197,7 +198,7 @@ def hspice_verification(inp):
 def main():
     f = "optimization.log"
     write_log_file(f, f" - - - -{datetime.now()} - - - - - -", 'w')
-    gm_id_optimizer([10,19,10])    
+    gm_id_optimizer([10, 19, 10])
     res = run_optimization()
     gm_id_optimizer(res.x, final=False)
 
@@ -209,13 +210,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
 
-    # inp = {}
-    # inp["fins"] = [26, 84, 49]
-    # inp["Is"] =  0.000452
-    # inp["stacks"] = [1, 1, 1]
-    # hspice_verification(inp)
+    inp = {}
+    inp["fins"] = [6, 16, 2]
+    inp["Is"] =  24e-6
+    inp["stacks"] = [1, 1, 1]
+    hspice_verification(inp)
    
 
 
